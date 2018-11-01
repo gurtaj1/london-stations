7230fe2c2d7db97f793f901818d4deac81b87a58
gurtaj1 <gurtaj@gmail.com>
2018-11-01 12:40:30 +0000

    modify component structs and create Station component

---
+++ app/components/stations/stations.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from 'components/stations/stations.scss';
+
+/**
+ * Station Component
+ * @param {name, lines} param0
+ */
+export default function Station ({
+  name,
+  lines
+}) {
+  return (
+    <Fragment>
+      <div className={styles['station-name']}>
+        {name}
+      </div>
+      <div className={styles['lines-list']}>
+        {
+          lines.map(line => line)
+        }
+      </div>
+    </Fragment>
+  );
+}
+
+Station.propTypes = {
+  name: PropTypes.string.isRequired,
+  lines: PropTypes.arrayOf(PropTypes.string).isRequired
+};
